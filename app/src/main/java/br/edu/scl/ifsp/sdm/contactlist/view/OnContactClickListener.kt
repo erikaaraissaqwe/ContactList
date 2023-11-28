5467711f172226b04c1147c0f6e1e29b2af54c49
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2023-11-28 16:04:25 -0300

    Implementação da lista de contatos.

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/OnContactClickListener.kt
@@ -0,0 +1,10 @@
+package br.edu.scl.ifsp.sdm.contactlist.view
+
+interface OnContactClickListener {
+
+    fun onContactClick( position: Int)
+
+    fun onRemoveContactMenuItemClick(position: Int)
+
+    fun onEditContactMenuItemClick(position: Int)
+}
